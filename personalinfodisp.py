7c3781a6d721110f9650dac5e11798b36049e9b0
Kankara Vandana <vandanakankara@gmail.com>
2025-12-26 13:55:05 +0530

    Add files via upload

---
+++ personalinfodisp.py
@@ -0,0 +1,15 @@
+# Type Content here...
+# Read inputs
+
+name = input()
+address = input()
+email = input()
+phone = input()
+
+# Print outputs
+
+print("personal information of user:")
+print("Name:", name)
+print("Address:", address)
+print("Email:", email)
+print("Phone:", 
